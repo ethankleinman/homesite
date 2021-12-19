3df43f4775ae071b915ed85c59604ad1ad4dd76a
Klein <ethankleinman@gmail.com>
2021-12-19 19:59:58 +0000

    test html file

--- gin/main.go
+++ gin/main.go
@@ -1,13 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+    "github.com/gin-gonic/gin"
+    "net/http"
+)
 
 func main() {
     r := gin.Default()
+    r.LoadHTMLGlob("templates/*")
     r.GET("/", func(c *gin.Context) {
-        c.JSON(200, gin.H{
-            "message": "pong",
-        })
+        c.HTML(http.StatusOK, "index.html", gin.H{})
     })
-    r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+    r.Run()
 }
